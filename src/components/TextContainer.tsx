abb1b23cda73f0ca73a3ee55f2c782649a8eac2e
REDDOTDIGITALIT\khairun.ayve <khairun.ayve@reddotdigitalit.com>
2025-02-09 18:25:12 +0600

    wip

--- src/components/TextContainer.tsx
+++ src/components/TextContainer.tsx
@@ -5,10 +5,40 @@ import { ReactComponent as WatchIcon } from "../images/watch.svg";
 
 const TextContainer = () => {
   return (
-    <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', mx: 'auto'}}>
-        <Typography sx={{color: '#202939', fontSize: '36px', fontWeight: '700', textAlign: 'center'}}>About Binge</Typography>
-        <Typography sx={{color: '#697586', fontSize: '16px', fontWeight: '400', textAlign: 'center'}}>Binge is an online video streaming platform that offers endless entertainment of Web Series, Bangla Dubbed Foreign Drama, Classic Hollywood Movies.</Typography>
-    </Box>
+    <div style={{ fontFamily: "Manrope" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          mx: "auto",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "#202939",
+            fontSize: "36px",
+            fontWeight: "700",
+            textAlign: "center",
+          }}
+        >
+          About Binge
+        </Typography>
+        <Typography
+          sx={{
+            color: "#697586",
+            fontSize: "16px",
+            fontWeight: "400",
+            textAlign: "center",
+          }}
+        >
+          Binge is an online video streaming platform that offers endless
+          entertainment of Web Series, Bangla Dubbed Foreign Drama, Classic
+          Hollywood Movies.
+        </Typography>
+      </Box>
+    </div>
   );
 };
 export default TextContainer;
